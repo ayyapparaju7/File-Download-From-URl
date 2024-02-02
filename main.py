85987ef66208875934c0f9ee352a96c03dbfd1ea
Ayyappa Raju Vatsavai <ayyapparaju7@gmail.com>
2024-02-02 21:39:20 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import openpyxl
+import  wget
+
+path = "C:\\Users\\ayyap\\PycharmProjects\\download\\LRS.xlsx"
+wb_obj = openpyxl.load_workbook(path)
+sheet_obj = wb_obj.active
+for i in range(1, 1047):
+    filename = sheet_obj.cell(row = i, column = 1).value
+    downloadUrl = sheet_obj.cell(row=i, column=2).value
+    response = wget.download(downloadUrl, filename)
+    print(i)
